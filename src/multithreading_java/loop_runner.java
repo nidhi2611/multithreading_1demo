62320e8f222708c2cd11c32725eb4db378a3d323
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-10 17:54:01 +0530

    thread called

--- src/multithreading_java/loop_runner.java
+++ src/multithreading_java/loop_runner.java
@@ -2,6 +2,8 @@ package multithreading_java;
 
 public class loop_runner {
     public static void main(String[] args) {
+        loop_creater obj = new loop_creater("hey ");
+        obj.start();
 
     }
 
