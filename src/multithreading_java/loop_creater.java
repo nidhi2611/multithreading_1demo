3acb79610d0d67c3b994ef0b32951089d6a53758
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-10 17:52:42 +0530

    run method overrided

--- src/multithreading_java/loop_creater.java
+++ src/multithreading_java/loop_creater.java
@@ -5,4 +5,16 @@ public class loop_creater extends Thread {
         super(name);
     }
 
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getName() + i);
+            System.out.print("done with loops");
+            try {
+                currentThread().sleep(2000);
+            } catch (InterruptedException e) {
+                System.out.println("interrupt aagyi");
+            }
+        }
+    }
 }
